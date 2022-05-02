c48caf15dbf8ff07050efd1c989c4b02265f290c
vionaosiako <viona.osiako@student.morigaschool.com>
2022-05-02 21:53:16 +0300

    Remove comments

--- config.py
+++ config.py
@@ -4,15 +4,15 @@ class Config:
 
     NEWS_API_SOURCES_URL = 'https://newsapi.org/v2/sources?apiKey={}'
     NEWS_API_ARTICLES_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
-    NEWS_API_KEY = '94d343343a714ec2b5f6a024996fe7d2'
-    SECRET_KEY='Flask WTF Secret Key'
+    NEWS_API_KEY = '11fc10d8188549cfad1539f1ad053fce'
+    SECRET_KEY='vivioonana'
 
 class ProdConfig(Config):
     pass
 
 
 class DevConfig(Config):
-    DEBUG = True
+DEBUG = True
 
 config_options = {
 'development':DevConfig,
